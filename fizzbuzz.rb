56a6087de0937e1de39dc3b690607ecf2c4023d9
Alberto Ochoa <ochoapublic@gmail.com>
2018-06-16 01:37:14 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(user_input)
+  reponse = ""
   if user_input % 3 == 0
     return "Fizz"
   end
